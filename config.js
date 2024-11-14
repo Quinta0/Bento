ff0c8d0bd6bca37f3c92f2b3363d6d2332100421
Quinta <112334784+Quinta0@users.noreply.github.com>
2024-11-14 10:53:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	//weatherKey: weatherKey, // Write here your API Key
+	weatherKey: 3ce750c03ffe22c772c84c8bc43a3eec, // Write here your API Key
 	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
